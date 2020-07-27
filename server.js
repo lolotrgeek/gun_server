7ebc244b94b8dcaf4fc615d6401082b69410e82d
lolotrgeek <lolotrgeek@gmail.com>
2020-07-27 08:55:15 -0500

    remove sql

--- server.js
+++ server.js
@@ -1,45 +1,40 @@
 ; (function () {
-	const cluster = require('cluster');
+	const cluster = require('cluster')
 	if (cluster.isMaster) {
-		return cluster.fork() && cluster.on('exit', function () { cluster.fork() });
+		return cluster.fork() && cluster.on('exit', function () { cluster.fork() })
 	}
 
-	const fs = require('fs');
+	const fs = require('fs')
 	const config = {
 		port: process.env.PORT || 8765,
 		host: '0.0.0.0'
 	};
 	const Gun = require('gun')
-	const GunSQLite = require('gun-sqlite');
-	const adapter = GunSQLite.bootstrap(Gun);
 
 	if (process.env.HTTPS_KEY) {
-		config.key = fs.readFileSync(process.env.HTTPS_KEY);
-		config.cert = fs.readFileSync(process.env.HTTPS_CERT);
-		config.server = require('https').createServer(config, Gun.serve(__dirname));
+		config.key = fs.readFileSync(process.env.HTTPS_KEY)
+		config.cert = fs.readFileSync(process.env.HTTPS_CERT)
+		config.server = require('https').createServer(config, Gun.serve(__dirname))
 	} else {
-		config.server = require('http').createServer(Gun.serve(__dirname));
+		config.server = require('http').createServer(Gun.serve(__dirname))
 	}
 
 	console.log('GUN config ', config)
-	const gun = Gun({
-		web: config.server.listen(config.port, config.host),
-		file: false,
-		radisk: false,
-		localStorage: false,
-		// Defaults
-		sqlite: {
-			database_name: "GunDB.db",
-		}
-	});
-	console.log('Relay peer started on port ' + config.port + ' with /gun');
+	const gun = Gun({ web: config.server.listen(config.port, config.host) })
+	console.log('Relay peer started on port ' + config.port + ' with /gun')
 
-	(function () {
-		setInterval(() => {
-			let peers = gun.back('opt.peers')
-			console.log(peers)
-		}, 7000)
-	})();
+	// ;(function () {
+	// 	setInterval(() => {
+	// 		let peers = gun.back('opt.peers')
+	// 		console.log(peers)
+	// 	}, 3000)
+	// })();
 	
-	module.exports = gun;
+	gun.get('tabs').on((data, key) => {
+		let peers = gun.back('opt.peers')
+		console.log(peers)
+	})
+
+
+	module.exports = gun
 }());
