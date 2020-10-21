b6de2cda8dc156bc4ddfb253100173e6573eaccc
lolotrgeek <lolotrgeek@gmail.com>
2020-10-21 09:37:51 -0500

    remove runChain

--- Export.js
+++ Export.js
@@ -18,46 +18,11 @@ const trimSoul = data => {
     delete data['_']
     return data
 }
-const saveFile = obj => {
-    fs.writeFile("filename.json", JSON.stringify(obj), "utf8", () => {
-        console.log('Written to File.')
-    })
-}
+
 function printProgress(progress) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     process.stdout.write(progress.toString());
-}
-
-const runChain = ({ key, final, count, total }) => {
-    if (!key) { console.log('No Key'); return false }
-    if (!count) count = 0
-    if (!total) total = []
-    if (!final) final = {}
-    // if(Object.keys(final).length === 0) final[key] = {}
-    // debug && console.log('Getting Key: ', key)
-    gun.get(key).once((current, currentkey) => {
-        if (current === null || current === undefined) { debug && console.log(' [End of Chain]'); return false }
-        current = trimSoul(current)
-        debug && console.log('Moving Down Chain:', currentkey, current)
-        final[key] = current
-        debug && console.log(final)
-        let values = Object.values(current)
-        debug && console.log(values)
-        // total.push(values.length)
-        let log = `${count},${key},${JSON.stringify(current)}\n`
-        fs.appendFile(logfile, log, "utf8", () => { })
-        fs.writeFile(output, JSON.stringify(final), "utf8", () => { })
-        let check = values.some(value => value && value['#'] && typeof value['#'] === 'string')
-        if (check) {
-            values.forEach(value => {
-                debug && console.log('Getting next: ', value['#'])
-                ++count
-                runChain({ key: value['#'], final, count, total })
-            })
-        }
-        printProgress(count + ' - ' + key)
-    })
 }
 
 const recursiveCallBase = (index, values) => {
